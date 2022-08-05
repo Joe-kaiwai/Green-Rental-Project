b80c231011332b6a88eedec36cd80727554f4a2e
Joe-KW <kaiwaing1722@gmail.com>
2022-08-05 15:23:35 +0800

    added order fliter function

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-// $dbConnection = mysqli_connect("localhost","root","","green_rental_project");
+ //$dbConnection = mysqli_connect("localhost","root","","green_rental_project");
 $dbConnection = mysqli_connect("remotemysql.com","mY7D8u4FAO","bj4MWMsIj2","mY7D8u4FAO");
 if(mysqli_connect_errno()){
     echo 'Failed to connect to MySQL';
